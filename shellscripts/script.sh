1a6fd8951c882affdb3407fa12bae45e159f0a1b
Martin Kroißenbrunner <104841196+martinkroissenbrunner@users.noreply.github.com>
2024-10-07 19:40:33 +0200

    script location handling

--- shellscripts/script.sh
+++ shellscripts/script.sh
@@ -3,8 +3,7 @@
 # hacky way to make shure the script 
 # gets allways run from parent-dir 
 # so relative paths get resolved the righ way
-script_location_dir="${BASH_SOURCE[0]}"
-cd $script_location_dir && cd ..
+cd "$(dirname "${BASH_SOURCE[0]}")" && cd ..
 # run scripts
 ./shellscripts/dl_saxon.sh
 ./shellscripts/dl_imprint.sh
